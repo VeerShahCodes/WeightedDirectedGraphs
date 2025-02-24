925163a9a437f6e97a790fc4d53ab7c60e6999d5
VeerShahCodes <Veer.Shah@GMR-118-1-2>
2025-02-23 17:31:53 -0800

    Made breadth first path find, now make depth first and compare

--- WeightedDirectedGraphs/Graph.cs
+++ WeightedDirectedGraphs/Graph.cs
@@ -34,7 +34,7 @@ namespace WeightedDirectedGraphs
             vertices = new List<Vertex<T>>();
         }
 
-        public void AddVertex(Vertex<T>? vertex)
+        private void AddVertex(Vertex<T>? vertex)
         {
             if(vertex != null && vertex.NeighborCount == 0 && vertex.Owner != this)
             {
@@ -43,20 +43,35 @@ namespace WeightedDirectedGraphs
             }
         }
 
-        public bool RemoveVertex(Vertex<T>? vertex)
+        public void AddVertex(T val)
+        {
+            AddVertex(new Vertex<T>(val));
+        }
+
+        private bool RemoveVertex(Vertex<T>? vertex)
         {
             if(vertex == null || vertex.Owner != this) return false;
 
-            for(int i = 0; i < vertices.Count; i++)
+            for(int i = 0; i < Edges.Count; i++)
             {
-                
+                if (Edges[i].EndingPoint == vertex)
+                {
+                    Edges[i].StartingPoint.Neighbors.Remove(Edges[i]);
+                }
             }
+            vertices.Remove(vertex);
             return true;
         }
 
-        public void AddEdge(Vertex<T>? a, Vertex<T>? b, float distance)
+        public bool RemoveVertex(T val)
         {
-            if(a != null && b != null && a.Owner != this && b.Owner != this)
+            bool b = RemoveVertex(Search(val));
+            return b;
+        }
+
+        private void AddEdge(Vertex<T>? a, Vertex<T>? b, float distance)
+        {
+            if(a != null && b != null && a.Owner == this && b.Owner == this)
             {
                 Edge<T> edge = new Edge<T>(a, b, distance);
                 if(!a.Neighbors.Contains(edge))
@@ -66,11 +81,121 @@ namespace WeightedDirectedGraphs
             }
         }
 
-        public bool RemoveEdge(Edge<T> edge)
+        public void AddEdge(T val1, T val2, float distance)
         {
-            if(edge.StartingPoint == null  ||  edge.EndingPoint == null || !Edges.Contains(edge)) return false;
-            edge.SelfDestruct();
+            AddEdge(Search(val1), Search(val2), distance);
+        }
+
+        
+
+        private bool RemoveEdge(Vertex<T>? a, Vertex<T>? b)
+        {
+            if (a == null || b == null) return false;
+            bool hasThatEdge = false;
+            int index = 0;
+            for(int i = 0; i < Edges.Count; i++)
+            {
+                if (Edges[i].StartingPoint == a && Edges[i].EndingPoint == b)
+                {
+                    hasThatEdge = true;
+                    index = i;
+                }
+            }
+            if (hasThatEdge == false) return false;
+
+            a.Neighbors.Remove(Edges[index]);
             return true;
+        }
+
+        public bool RemoveEdge(T val1, T val2)
+        {
+            bool b = RemoveEdge(Search(val1), Search(val2));
+            return b;
+        }
+
+        public Vertex<T>? Search(T? value)
+        {
+            if (value == null) return null;
+            int z = -1;
+
+            for(int i = 0; i < vertices.Count; i++)
+            {
+                if (value.Equals(vertices[i].Value))
+                {
+                    z = i;
+                    break;
+                }
+            }
+
+            if (z == -1) return null;
+            else
+            {
+                return vertices[z];
+            }
+
+        }
+
+        public Edge<T>? GetEdge(Vertex<T>? a, Vertex<T>? b)
+        {
+            if (a == null || b == null) return null;
+            bool hasThatEdge = false;
+            int index = 0;
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                if (Edges[i].StartingPoint == a && Edges[i].EndingPoint == b)
+                {
+                    hasThatEdge = true;
+                    index = i;
+                }
+            }
+            if (hasThatEdge == false) return null;
+
+            return Edges[index];
+        }
+
+        public List<Vertex<T>>? PathFind(Vertex<T>? start, Vertex<T>? end)
+        {
+            if (start == null || end == null) return null;
+            float pathCost = 0f;
+
+            Dictionary<Vertex<T>, Vertex<T>?> previousVertex = new()
+            {
+                [start] = null
+            };
+
+            Queue<Vertex<T>> queue = new Queue<Vertex<T>>();
+            queue.Enqueue(start);
+            while(queue.Count > 0)
+            {
+                Vertex<T>? current = queue.Dequeue();
+                if(current.Equals(end))
+                {
+                    List<Vertex<T>> path = new List<Vertex<T>>();
+                    while (current != null)
+                    {
+                        path.Add(current);
+                        if(GetEdge(previousVertex[current], current) != null)
+                        {
+                            pathCost += GetEdge(previousVertex[current], current).Distance;
+
+                        }
+                        current = previousVertex[current];
+                    }
+                    path.Reverse();
+                    Console.WriteLine("Breadth First: " + pathCost);
+                    return path;
+                }
+
+                for (int i = 0; i < current.NeighborCount; i++)
+                {
+                    if (!previousVertex.ContainsKey(current.Neighbors[i].EndingPoint))
+                    {
+                        queue.Enqueue(current.Neighbors[i].EndingPoint);
+                        previousVertex[current.Neighbors[i].EndingPoint] = current;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
--- WeightedDirectedGraphs/Program.cs
+++ WeightedDirectedGraphs/Program.cs
@@ -4,8 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            
+            Graph<int> graph = new Graph<int>();
+
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddVertex(7);
+            graph.AddVertex(8);
+
+            graph.AddEdge(1, 2, 4.3f);
+            graph.AddEdge(1, 3, 6.7f);
+            graph.AddEdge(1, 4, 1.2f);
+            graph.AddEdge(2, 5, 4.8f);
+            graph.AddEdge(5, 1, 20f);
+            graph.AddEdge(2, 8, 15f);
+            graph.AddEdge(1, 8, 21f);
+            graph.AddEdge(4, 7, 5f);
+            graph.AddEdge(3, 6, 2.4f);
+
+            List<Vertex<int>>? list = graph.PathFind(graph.Search(1), graph.Search(5));
+            ;
+
         }
     }
 }
